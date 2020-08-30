4ebd952b7a9228bc066018900782cd7d7bd07f9b
Minsuk Ji <37008281+minsukji@users.noreply.github.com>
2020-08-30 14:09:14 -0400

    Now cancel.yml handles all cancellations including checking skip-ci (#29)
    * now cancel.yml handles all cancellations
    
    Co-authored-by: MinsukJi-NOAA <Minsuk.Ji@noaa.gov>

--- json_helper.py
+++ json_helper.py
@@ -7,19 +7,14 @@ import sys
 import json
 
 def check_skip(data):
-  ub=min(10, len(data))
-  msg=[x["head_commit"]["message"] for x in data[:ub] if x["id"]==int(os.environ["GITHUB_RUN_ID"])]
-
-  if re.search("skip-ci", msg[0]):
-    return 'yes'
-  return 'no'
+  msg = data["head_commit"]["message"]
+  if re.search("skp-ci", msg):
+    return data["id"]
+  else:
+    return -1
 
 def cancel_workflow(data):
-  # GITHUB_ACTOR is the same 
   # GITHUB_HEAD_REF is the same 
-  # GITHUB_RUN_ID is different (or GITHUB_SHA is different)
-  # event is pull_request
-  # either queued or in_progress
   wfs=[x["id"] for x in data if x["head_repository"] is not None and
         re.search(os.environ["GITHUB_ACTOR"], x["head_repository"]["owner"]["login"]) and
         x["head_branch"]=="master" and x["id"]!=int(os.environ["GITHUB_RUN_ID"]) and
@@ -28,16 +23,19 @@ def cancel_workflow(data):
   return wfs
 
 def main():
-  data = json.load(sys.stdin)["workflow_runs"]
 
   if sys.argv[1]=="check_skip":
-    answer=check_skip(data)
-    print(answer)
+    data = json.load(sys.stdin)["workflow_run"]
+    cancel_id = check_skip(data)
+    print(cancel_id)
   elif sys.argv[1]=="cancel_workflow":
+    data = json.load(sys.stdin)["workflow_runs"]
     wfs = cancel_workflow(data)
     if len(wfs)==0:
       print("")
     else:
       print(*wfs)
+  else:
+    print("ERROR")
 
 if __name__ == "__main__": main()
